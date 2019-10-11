35da67f294a6950ffc0a70fa616cf753ea0f534e
windyautumn <53567667+windyautumn@users.noreply.github.com>
2019-10-11 21:18:45 +0800

    首页

---
+++ pages/air/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      air
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/hotel/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      hotel
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
